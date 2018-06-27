16c70b2a1770efa5b77fa5309f369e373ecad6ff
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-06-27 21:31:45 +0300

    Production container will run without docker-compose

--- scripts/docker_production_start.sh
+++ scripts/docker_production_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}/.."
-
-docker-compose -f docker-compose-prod.yml pull
-docker-compose -f docker-compose-prod.yml up -d
+docker pull hashdivision/clothobserve:latest
+docker run -p 127.0.0.1:5000:5000 -e CONFIG_OBJECT=configs.config.ProductionConfig -e ROOT_PATH=/clothobserve/ --restart=always -d hashdivision/clothobserve:latest
