131c1ace44fca7b893d2b9d3275419e749bbbb7e
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-06-27 22:02:46 +0300

    Added script for testing in Docker

---
+++ scripts/docker_test_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+docker pull hashdivision/clothobserve:development
+docker run --name mongodb -e MONGODB_USERNAME=clothobserveadmin -e MONGODB_PASSWORD=clothobserveadmin -e MONGODB_DATABASE=clothobserve -d bitnami/mongodb:latest
+docker run -it -rm --link=mongodb:mongodb -e CONFIG_OBJECT=configs.config.TestingConfig -e ROOT_PATH=/clothobserve/ -v ../tests:/tests -v ../docs:/docs hashdivision/clothobserve:development
+docker stop mongodb
+docker rm mongodb
