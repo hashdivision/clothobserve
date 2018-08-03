6da669f5318eb2be934964d2690f2037435ff827
Harry Sky <harry.sky.vortex@gmail.com>
2018-08-03 12:14:00 +0300

    Fixes docker scripts for production and test
    Scripts pull needed images from Docker Hub
    docker_test_start.sh now removes containers in the end

--- scripts/docker_test_start.sh
+++ scripts/docker_test_start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+function finish {
+    docker stop mongodb
+    docker rm mongodb
+}
+trap finish EXIT
+
+
 docker pull hashdivision/clothobserve:development
 docker run --name mongodb -e MONGODB_USERNAME=clothobserveadmin -e MONGODB_PASSWORD=clothobserveadmin -e MONGODB_DATABASE=clothobserve -d bitnami/mongodb:latest
-docker run -it -rm --link=mongodb:mongodb -e CONFIG_OBJECT=configs.config.TestingConfig -e ROOT_PATH=/clothobserve/ -v ../tests:/tests -v ../docs:/docs hashdivision/clothobserve:development
-docker stop mongodb
-docker rm mongodb
+docker run -it --rm --link=mongodb:mongodb -e CONFIG_OBJECT=configs.config.TestingConfig -e ROOT_PATH=/clothobserve/ -v tests:/tests -v docs:/docs hashdivision/clothobserve:development
