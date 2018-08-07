5ec5c2c7154d74091999bddc3e163035532dfbf3
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-08-07 14:44:04 +0300

    Adds new configurations and scripts
    - Script docker_test_start.sh should be run only when checking
    if everything in current branch is great (tests, docs and lint)
    
    - Relatively fast configuration for testing added and it should
    be used for testing (script docker_quicktest_start.sh)
    
    - Configuration for generating documentation now has separate
    script too!

--- scripts/docker_quicktest_start.sh
+++ scripts/docker_quicktest_start.sh
@@ -11,5 +11,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/.."
 
 docker run --name mongodb -e MONGODB_USERNAME=clothobserveadmin -e MONGODB_PASSWORD=clothobserveadmin -e MONGODB_DATABASE=clothobserve -d bitnami/mongodb:latest
-docker build -f Dockerfile-Test -t clothobserve-test .
-docker run -it --rm --link=mongodb:mongodb -e CONFIG_OBJECT=configs.config.TestingConfig -e ROOT_PATH=/clothobserve/ -e MONGODB_PORT=27017 -v ${PWD}/tests:/tests -v ${PWD}/docs:/docs clothobserve-test
+docker build -f Dockerfile-Quicktest -t clothobserve-quicktest .
+docker run -it --rm --link=mongodb:mongodb -e CONFIG_OBJECT=configs.config.TestingConfig -e ROOT_PATH=/clothobserve/ -e MONGODB_PORT=27017 -v ${PWD}/clothobserve:/clothobserve -v ${PWD}/tests:/tests clothobserve-quicktest
