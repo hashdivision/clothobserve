50cde8dea1fbff5755a704d9607926557bc44c32
Igor Nehoroshev <igor.nehoroshev@martem.ee>
2018-08-08 18:49:18 +0300

    Adds Development Local configuration and script
    - Dockerfile for Production: fixed case of server variable

---
+++ scripts/docker_dev_local_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+function finish {
+    docker stop mongodb
+    docker rm mongodb
+}
+trap finish EXIT
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}/.."
+
+docker run --name mongodb -e MONGODB_USERNAME=clothobserveadmin -e MONGODB_PASSWORD=clothobserveadmin -e MONGODB_DATABASE=clothobserve -d bitnami/mongodb:latest
+docker build -f Dockerfile-Dev-Local -t clothobserve-dev-local .
+docker run -it -p 127.0.0.1:80:5000 -e CONFIG_OBJECT=configs.config.DevelopmentLocalConfig -e ROOT_PATH=/clothobserve/ -e MONGODB_PORT=27017 -v ${PWD}/clothobserve:/clothobserve clothobserve-dev-local
