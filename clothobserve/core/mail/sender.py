70199226071ea26e5a8d75e7ec62452b63a25874
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-08-10 11:45:43 +0300

    Add method for sending HTML mail

--- clothobserve/core/mail/sender.py
+++ clothobserve/core/mail/sender.py
@@ -10,8 +10,19 @@
     |
 
 """
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
 #: Flask-Mail instance to send mail.
 MAIL = Mail()
 
+def send_html_mail(subject: str, body_html: str, recipient: str) -> None:
+    """
+    Sends HTML mail to one recipient.
+
+    :param subject: subject of the email.
+    :param body_html: HTML body.
+    :param recipient: who will receive this message.
+    """
+    message = Message(subject, recipients=[recipient])
+    message.html = body_html
+    MAIL.send(message)
