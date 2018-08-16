ffc1924ceaad06a06e69aacd31658eb288113c56
Anton Tkach <dekerter@gmail.com>
2018-08-16 12:13:44 +0300

    Add user registration endpoint

--- clothobserve/core/users/account.py
+++ clothobserve/core/users/account.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, current_app
 from flask_security.core import current_user
 from flask_security.utils import logout_user, verify_password
 from flask_api import status
-from core.database.user_models import User
+from core.database.user_models import User, USER_DATASTORE
 
 ACCOUNT_BP = Blueprint("account", __name__)
 
@@ -12,8 +12,39 @@ WRONG_CREDENTIALS = Response("Wrong Credentials", status=status.HTTP_401_UNAUTHO
 SIGNIN_SUCCESS = Response("User Signed In", status=status.HTTP_200_OK)
 SIGNIN_BAD_REQUEST = Response("Bad Request", status=status.HTTP_400_BAD_REQUEST)
 
+NOT_FOUND = Response ("Not Found", status=status.HTTP_404_NOT_FOUND)
+CAPTCHA_FAIL = Response ("Captcha fail", status=status.HTTP_401_UNAUTHORIZED)
+REGISTRATION_SUCCESSFUL = Response("Registered Successfully", status=status.HTTP_200_OK)
+EMAIL_IS_REGISTERED = Response("Email Is Already Registered", status=status.HTTP_401_UNAUTHORIZED)
+REGISTRATION_BAD_REQUEST = Response("Bad Request", status=status.HTTP_400_BAD_REQUEST)
+
 LOGOUT_SUCCESS = Response("Logout Success", status=status.HTTP_200_OK)
 LOGOUT_FAIL = Response("Logout Fail", status=status.HTTP_401_UNAUTHORIZED)
+
+@ACCOUNT_BP.route("/register", methods=['POST'])
+def register_endpoint() -> Response:
+	"""
+	# TODO: Fill this docstring.
+	"""
+	if current_user.is_authenticated:
+		return NOT_FOUND
+
+	if not current_app.config['RECAPTCHA'] or "g-recaptcha-response" in request.form:
+        if current_app.config['RECAPTCHA'] and \
+            not is_recaptcha_passed(request.form["g-recaptcha-response"]):
+            return CAPTCHA_FAIL # pragma: no cover 
+		
+		if "email" in request.form and "password" in request.form:
+            if create_new_user(request.form["email"], request.form["password"]):
+                user = User.find_by_email(request.form["email"])
+                send_confirmation_link(user)
+                login_user(user, remember=True)
+                return REGISTRATION_SUCCESSFUL
+
+            return EMAIL_IS_REGISTERED
+	
+	return REGISTRATION_BAD_REQUEST
+
 
 @ACCOUNT_BP.route("/signin", methods=['POST'])
 def signin_endpoint() -> Response:
