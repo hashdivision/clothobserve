37bb8d336aa78ebb68b2711f9c7edee017af6468
Igor Nehoroshev <igor.nehoroshev@martem.ee>
2018-08-18 17:17:00 +0300

    Add admin endpoint to delete user

--- clothobserve/core/admin/users.py
+++ clothobserve/core/admin/users.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, Response, request
 from flask_api import status
 from core.decorators.auth import login_required, roles_required
-from core.database.user_models import USER_DATASTORE
+from core.database.user_models import User, USER_DATASTORE
 from utils.responses import SUCCESS, BAD_REQUEST
 
 ADMIN_USERS_BP = Blueprint("admin/users", __name__)
 
 EMAIL_IS_REGISTERED = Response("Email Is Registered", status=status.HTTP_401_UNAUTHORIZED)
+USER_NOT_FOUND = Response("User Not Found", status=status.HTTP_404_NOT_FOUND)
 
 @login_required(silent=true)
 @roles_required("admin", silent=true)
@@ -25,5 +26,22 @@ def register_endpoint() -> Response:
             return SUCCESS
 
         return EMAIL_IS_REGISTERED
+
+	return BAD_REQUEST
+
+@login_required(silent=true)
+@roles_required("admin", silent=true)
+@ADMIN_USERS_BP.route("/delete", methods=['POST'])
+def register_endpoint() -> Response:
+	"""
+	# TODO: Fill this docstring.
+	"""
+	if "email" in request.form:
+        user = User.find_by_email(request.form["email"])
+        if not user:
+            return USER_NOT_FOUND
+
+        USER_DATASTORE.delete_user(user)
+        return SUCCESS
 
 	return BAD_REQUEST
