b52b73ccef4fc131af5f8cdfc80acd36b27cfbc7
Igor Nehoroshev <igor.nehoroshev@martem.ee>
2018-08-18 15:52:31 +0300

    Change responses endpoints give back

--- clothobserve/core/users/account.py
+++ clothobserve/core/users/account.py
@@ -3,22 +3,13 @@ from flask_security.core import current_user
 from flask_security.utils import logout_user, login_user, verify_password
 from flask_api import status
 from core.database.user_models import User, USER_DATASTORE
+from utils.responses import SUCCESS, BAD_REQUEST, NOT_FOUND
 
 ACCOUNT_BP = Blueprint("account", __name__)
 
-SIGNED_IN = Response(status=status.HTTP_204_NO_CONTENT)
+EMAIL_IS_REGISTERED = Response("Email Is Registered", status=status.HTTP_401_UNAUTHORIZED)
 USER_INACTIVE = Response("User Is Inactive", status=status.HTTP_403_FORBIDDEN)
 WRONG_CREDENTIALS = Response("Wrong Credentials", status=status.HTTP_401_UNAUTHORIZED)
-SIGNIN_SUCCESS = Response("User Signed In", status=status.HTTP_200_OK)
-SIGNIN_BAD_REQUEST = Response("Bad Request", status=status.HTTP_400_BAD_REQUEST)
-
-NOT_FOUND = Response ("Not Found", status=status.HTTP_404_NOT_FOUND)
-REGISTRATION_SUCCESS = Response("Registered Successfully", status=status.HTTP_200_OK)
-EMAIL_IS_REGISTERED = Response("Email Is Already Registered", status=status.HTTP_401_UNAUTHORIZED)
-REGISTRATION_BAD_REQUEST = Response("Bad Request", status=status.HTTP_400_BAD_REQUEST)
-
-LOGOUT_SUCCESS = Response("Logout Success", status=status.HTTP_200_OK)
-LOGOUT_FAIL = Response("Logout Fail", status=status.HTTP_401_UNAUTHORIZED)
 
 @ACCOUNT_BP.route("/register", methods=['POST'])
 def register_endpoint() -> Response:
@@ -29,39 +20,39 @@ def register_endpoint() -> Response:
 		return NOT_FOUND
 
 	if "email" in request.form and "password" in request.form:
-        if create_new_user(request.form["email"], request.form["password"]):
+        if USER_DATASTORE.create_new_user(request.form["email"], request.form["password"]):
             user = User.find_by_email(request.form["email"])
             login_user(user, remember=True)
-            return REGISTRATION_SUCCESS
+            return SUCCESS
 
         return EMAIL_IS_REGISTERED
 
-	return REGISTRATION_BAD_REQUEST
+	return BAD_REQUEST
 
 
 @ACCOUNT_BP.route("/signin", methods=['POST'])
 def signin_endpoint() -> Response:
     if current_user.is_authenticated:
-        return SIGNED_IN
+        return NOT_FOUND
 
     if "email" in request.form and "password" in request.form:
         user = User.find_by_email(request.form["email"])
         if user and verify_password(request.form["password"], user.password):
             if user.active:
                 login_user(user, remember=True)
-                return SIGNIN_SUCCESS
+                return SUCCESS
 
             return USER_INACTIVE
 
         return WRONG_CREDENTIALS
 
-    return SIGNIN_BAD_REQUEST
+    return BAD_REQUEST
 
 
 @ACCOUNT_BP.route("/logout")
 def logout_endpoint() -> Response:
 	if current_user.is_authenticated:
 		logout_user()
-		return LOGOUT_SUCCESS
+		return SUCCESS
 	
-	return LOGOUT_FAIL
+	return NOT_FOUND
