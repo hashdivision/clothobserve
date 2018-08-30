ee7d72d8696840fbc91f3af5281e1a4e8f0d6ca9
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-08-30 16:36:55 +0300

    Add endpoint for requesting other users' profiles

---
+++ clothobserve/data/constants/responses/user_profile.py
@@ -0,0 +1,17 @@
+"""
+    clothobserve.data.constants.responses.user_profile
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # TODO: Fill this docstring.
+
+    :copyright: © 2018 HashDivision OU.
+
+    :license: Apache License 2.0, see *LICENSE* for more details.
+
+    |
+
+"""
+from flask import Response
+from flask_api import status
+
+#: Response for user profile search that was unsuccessful.
+PROFILE_NOT_FOUND = Response("Profile Not Found", status=status.HTTP_404_NOT_FOUND)
---
+++ clothobserve/endpoints/user/profile.py
@@ -0,0 +1,33 @@
+"""
+    clothobserve.endpoints.user.profile
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # TODO: Fill this docstring.
+
+    :copyright: © 2018 HashDivision OU.
+
+    :license: Apache License 2.0, see *LICENSE* for more details.
+
+    |
+
+"""
+from flask import Blueprint, Response, request
+from endpoints.decorators.auth import login_required
+from data.models.user import User, Profile
+from data.constants.responses.user_profile import PROFILE_NOT_FOUND
+
+#: Blueprint of this profile module.
+PROFILE_BP = Blueprint("profile", __name__)
+
+@PROFILE_BP.route("/user/<username>")
+@login_required(silent=True)
+def user_endpoint(username: str) -> Response:
+    """
+    # TODO: Fill this docstring.
+    """
+    user = User.find_by_username(username)
+    if user:
+        profile = Profile.find_public_by_user(user)
+        if profile:
+            return profile.to_json()
+
+    return PROFILE_NOT_FOUND
