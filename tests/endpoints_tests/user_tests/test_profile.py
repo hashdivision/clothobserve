4ac3c02f51e08bd29fa041e17c4fa36875d7ca3d
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-08-30 16:37:54 +0300

    Add test for /profile/user/<username> endpoint

---
+++ tests/endpoints_tests/user_tests/test_profile.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+from flask import Response
+from flask.testing import FlaskClient
+from flask_api import status
+from main import SERVER
+from data.models.user import User
+from data.constants.responses.user_profile import PROFILE_NOT_FOUND
+from logic.user.datastore import USER_DATASTORE
+
+def user(client: FlaskClient, username: str) -> Response:
+    """Fast method for using ``/profile/user/<username>`` endpoint"""
+    return client.get('/profile/user/' + username)
+
+def register(client: FlaskClient, email: str, password: str) -> None:
+    """Fast method for using ``/account/register`` endpoint"""
+    form_data = 'email=' + email +'&password=' + password
+    client.post('/account/register', data=form_data, content_type='application/x-www-form-urlencoded')
+
+class UserProfileTestCase(unittest.TestCase):
+    """
+    # TODO: Fill this docstring.
+    """
+
+    __RANDOM_PROFILE_TEST_EMAIL = 'random.profile.test@example.com'
+    __RANDOM_PASSWORD = 'RandomPassword'
+
+    def test_user_endpoint(self):
+        """
+        # TODO: Fill this docstring.
+        """
+        with SERVER.test_client() as client:
+            admin_username = os.getenv('ADMIN_USERNAME', 'Admin')
+            admin_email = os.getenv('ADMIN_EMAIL', 'admin@example.com')
+            admin = User.find_by_email(admin_email)
+
+            register(client, self.__RANDOM_PROFILE_TEST_EMAIL, self.__RANDOM_PASSWORD)
+
+            USER_DATASTORE.change_profile_visibility(admin, public=True)
+            success_result = user(client, admin_username)
+            self.assertEqual(success_result.status_code, status.HTTP_200_OK)
+
+            USER_DATASTORE.change_profile_visibility(admin, public=False)
+            failed_result = user(client, admin_username)
+            self.assertEqual(failed_result.status_code, PROFILE_NOT_FOUND.status_code)
+            
