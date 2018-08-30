e9263205089de652074c2555849bb1b782e6b3e8
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-08-30 13:40:09 +0300

    Adapt /account endpoints to previous changes
    This makes code cleaner

--- clothobserve/endpoints/user/account.py
+++ clothobserve/endpoints/user/account.py
@@ -11,11 +11,10 @@
 
 """
 from flask import Blueprint, Response, request
-from flask_security.core import current_user
 from flask_security.utils import login_user, logout_user, verify_password
-from endpoints.decorators.auth import login_required
+from endpoints.decorators.auth import login_required, anonymous_required
+from endpoints.decorators.data import form_required
 from data.models.user import User
-from data.constants.responses.generic import NOT_FOUND, BAD_REQUEST
 from data.constants.responses.user_account import EMAIL_IS_REGISTERED, \
     USER_INACTIVE, WRONG_CREDENTIALS, LOGGED_OUT
 from logic.user.datastore import USER_DATASTORE
@@ -24,43 +23,35 @@ from logic.user.datastore import USER_DATASTORE
 ACCOUNT_BP = Blueprint("account", __name__)
 
 @ACCOUNT_BP.route("/register", methods=['POST'])
+@anonymous_required
+@form_required("email", "password")
 def register_endpoint() -> Response:
     """
     # TODO: Fill this docstring.
     """
-    if current_user.is_authenticated:
-        return NOT_FOUND
+    user = USER_DATASTORE.create_new_user(request.form["email"], request.form["password"])
+    if user:
+        login_user(user, remember=True)
+        return '{"username":"' + user.username + '"}'
 
-    if "email" in request.form and "password" in request.form:
-        if USER_DATASTORE.create_new_user(request.form["email"], request.form["password"]):
-            user = User.find_by_email(request.form["email"])
-            login_user(user, remember=True)
-            return '{"username":"' + user.username + '"}'
-
-        return EMAIL_IS_REGISTERED
-
-    return BAD_REQUEST
+    return EMAIL_IS_REGISTERED
 
 @ACCOUNT_BP.route("/signin", methods=['POST'])
+@anonymous_required
+@form_required("email", "password")
 def signin_endpoint() -> Response:
     """
     # TODO: Fill this docstring.
     """
-    if current_user.is_authenticated:
-        return NOT_FOUND
+    user = User.find_by_email(request.form["email"])
+    if user and verify_password(request.form["password"], user.password):
+        if user.active:
+            login_user(user, remember=True)
+            return '{"username":"' + user.username + '"}'
 
-    if "email" in request.form and "password" in request.form:
-        user = User.find_by_email(request.form["email"])
-        if user and verify_password(request.form["password"], user.password):
-            if user.active:
-                login_user(user, remember=True)
-                return '{"username":"' + user.username + '"}'
+        return USER_INACTIVE
 
-            return USER_INACTIVE
-
-        return WRONG_CREDENTIALS
-
-    return BAD_REQUEST
+    return WRONG_CREDENTIALS
 
 @ACCOUNT_BP.route("/logout")
 @login_required(silent=True)
