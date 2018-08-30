acda5d0e5139f632c59750370c592fc7c75176c1
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-08-30 16:33:48 +0300

    Add default username to admin user

--- clothobserve/logic/initialization.py
+++ clothobserve/logic/initialization.py
@@ -13,6 +13,7 @@
 import os
 from flask import Flask
 from endpoints.user.account import ACCOUNT_BP
+from endpoints.user.profile import PROFILE_BP
 from endpoints.admin.users import ADMIN_USERS_BP
 from logic.user.datastore import USER_DATASTORE
 
@@ -46,7 +47,6 @@ def create_default_user_roles() -> None:
     USER_DATASTORE.find_or_create_role(name='superuser', \
                                         description='Premium user of Clothobserve.')
 
-
 def create_admin_user() -> None:
     """
     Creates admin user.
@@ -54,12 +54,16 @@ def create_admin_user() -> None:
     """
     email = os.getenv('ADMIN_EMAIL', 'admin@example.com')
     password = os.getenv('ADMIN_PASSWORD', 'ChangeMeASAP')
-    USER_DATASTORE.create_new_user(email=email, password=password, \
-                                    role='admin', confirmed=True)
+    admin = USER_DATASTORE.create_new_user(email=email, password=password, \
+                                            role='admin', confirmed=True)
+
+    admin.username = os.getenv('ADMIN_USERNAME', 'Admin')
+    admin.save()
 
 def register_blueprints(server: Flask) -> None:
     """
     # TODO: Fill this docstring.
     """
     server.register_blueprint(ACCOUNT_BP, url_prefix="/account")
+    server.register_blueprint(PROFILE_BP, url_prefix="/profile")
     server.register_blueprint(ADMIN_USERS_BP, url_prefix="/admin/users")
