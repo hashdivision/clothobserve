2e99614d6535c895a567352238c66fa462f5fc4a
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-09-02 23:44:24 +0300

    Add response for successful change of profile visibility

--- clothobserve/data/constants/responses/user_profile.py
+++ clothobserve/data/constants/responses/user_profile.py
@@ -15,3 +15,5 @@ from flask_api import status
 
 #: Response for user profile search that was unsuccessful.
 PROFILE_NOT_FOUND = Response("Profile Not Found", status=status.HTTP_404_NOT_FOUND)
+#: Response for successful profile visibility change.
+VISIBILITY_CHANGE_SUCCESS = Response("Visibility Changed", status=status.HTTP_200_OK)
