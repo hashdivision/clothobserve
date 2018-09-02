58532c6153e5d323fdbafafbe9e8ec9ccb1135ed
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-09-02 23:40:42 +0300

    Add endpoint for profile visibility change

--- clothobserve/endpoints/user/profile.py
+++ clothobserve/endpoints/user/profile.py
@@ -14,7 +14,9 @@ from flask import Blueprint, Response
 from flask_security.core import current_user
 from endpoints.decorators.auth import login_required
 from data.models.user import User, Profile
-from data.constants.responses.user_profile import PROFILE_NOT_FOUND
+from data.constants.responses.user_profile import PROFILE_NOT_FOUND, \
+    VISIBILITY_CHANGE_SUCCESS
+from logic.user.datastore import USER_DATASTORE
 
 #: Blueprint of this profile module.
 PROFILE_BP = Blueprint("profile", __name__)
@@ -25,7 +27,7 @@ def root_profile_endpoint() -> Response:
     """
     # TODO: Fill this docstring.
     """
-    return Profile.find_by_user(current_user._get_current_object()).to_response_json()
+    return current_user.profile.to_response_json()
 
 @PROFILE_BP.route("/user/<username>")
 @login_required(silent=True)
@@ -34,9 +36,16 @@ def user_endpoint(username: str) -> Response:
     # TODO: Fill this docstring.
     """
     user = User.find_by_username(username)
-    if user:
-        profile = Profile.find_public_by_user(user)
-        if profile:
-            return profile.to_response_json()
+    if user and user.profile.public:
+        return user.profile.to_response_json()
 
     return PROFILE_NOT_FOUND
+
+@PROFILE_BP.route("/visibility/<int:state>")
+@login_required(silent=True)
+def visibility_endpoint(state: int) -> Response:
+    """
+    # TODO: Fill this docstring.
+    """
+    USER_DATASTORE.change_profile_visibility(current_user, public=bool(state))
+    return VISIBILITY_CHANGE_SUCCESS
