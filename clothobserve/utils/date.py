3eb0ac48d06bdca22447c65c20c4ae7bf0a8a7c5
Igor Nehoroshev <harry.sky.vortex@gmail.com>
2018-09-08 14:15:54 +0300

    Add method to convert date to string which can be used in JSON

--- clothobserve/utils/date.py
+++ clothobserve/utils/date.py
@@ -20,3 +20,12 @@ def convert_to_datetime(string: str, date_format: str = '%d.%m.%Y') -> datetime:
         return datetime.strptime(string, date_format)
     except ValueError:
         return None
+
+def convert_to_string(datetime_object: datetime) -> str:
+    """
+    # TODO: Fill this docstring.
+    """
+    try:
+        return datetime_object.strftime("%d.%m.%Y")
+    except AttributeError:
+        return "null"
