84809d3bbdae029835e3bf92934cb33d6f553890
Igor Nehoroshev <igor.nehoroshev@martem.ee>
2018-09-28 02:55:36 +0300

    Write proper password restoration email message

--- clothobserve/data/constants/mail/password.py
+++ clothobserve/data/constants/mail/password.py
@@ -10,6 +10,19 @@
     |
 
 """
+from os import getenv
+
+#: This URL + token will be sent to email of password restoring user.
+#: URL + token should give user a form with password input that POSTs to
+#: endpoint ``/account/password/restore/<token>``.
+_RESTORE_URL = getenv('PASSWORD_RESTORE_URL', 'https://example.com/restore')
+#: Clickable HTML link to password restoration.
+_RESTORATION_LINK = '<a href="' + _RESTORE_URL + '/%s">Restoration Link</a>'
+
 #: Message which will be sent to user's email upon restoration request.
 #: You should provide token via ``RESTORATION_MESSAGE % token``.
-RESTORATION_MESSAGE = "Restoration link: localhost/restore/%s"
+RESTORATION_MESSAGE = ("<b>Hello!</b></br>"
+                       "Password reset was requested for this email.</br>"
+                       "<u>Please ignore this email if You did not request it</u>.</br>"
+                       "Use link below to set new password (valid for 24 hours).</br>") \
+                       + _RESTORATION_LINK
